9289c71b9accf887910f607335a2742ffee34a1a
Florent Galland <florent.galland@gmail.com>
2013-03-28 21:48:14 +0100

    Try to allow cors

--- cabble/server.js
+++ cabble/server.js
@@ -17,6 +17,7 @@ You should have received a copy of the GNU Affero General Public License
 along with glark.io.  If not, see <http://www.gnu.org/licenses/>. */
 
 var config = require('./config.js');
+var cors = require('cors');
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -32,6 +33,12 @@ app.set('port', process.env.PORT || config.server.listenPort);
 
 // From now on, log everything.
 app.use(express.logger('dev'));
+
+// Allow cross origin resource sharing for ace cdn.
+var corsOptions = {
+    origin: 'http://d1n0x3qji82z53.cloudfront.net'
+};
+app.use(cors(corsOptions));
 
 // Serve static files.
 app.use(express.compress());
@@ -84,7 +91,7 @@ app.configure('production', function () {
 
 /* Route everything that was not answered yet to the main page. */
 // FIXME disable this when implementing the invite collab feature.
-app.all('*', function (req, res) {
+app.all('*', cors(corsOptions), function (req, res) {
     res.redirect('/');
 });
 
